9bc8ba2a55afc56e634d23e7cbbe7c8c0ed0bb87
Nexora Solutions <dacoheed@gmail.com>
2026-04-25 08:02:16 -0500

    Create users.js

---
+++ users.js
@@ -0,0 +1,34 @@
+// users.js
+function getUsers() {
+  return JSON.parse(localStorage.getItem("users")) || [];
+}
+
+function saveUsers(users) {
+  localStorage.setItem("users", JSON.stringify(users));
+}
+
+function initDefaultAdmin() {
+  let users = getUsers();
+
+  const exists = users.some(
+    u => u.username === "NEXORAADMIN" && u.badge === "9574268"
+  );
+
+  if (!exists) {
+    users.push({
+      username: "NEXORAADMIN",
+      password: "NEXORA-SYSTEM-CORE",
+      badge: "9574268",
+      isAdmin: true
+    });
+    saveUsers(users);
+  }
+}
+
+function setCurrentUser(user) {
+  localStorage.setItem("currentUser", JSON.stringify(user));
+}
+
+function getCurrentUser() {
+  return JSON.parse(localStorage.getItem("currentUser"));
+}
