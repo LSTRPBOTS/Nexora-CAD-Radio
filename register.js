be3d414860b356e1ce192b3cb7b376689b7a3091
Nexora Solutions <dacoheed@gmail.com>
2026-05-15 20:17:27 -0500

    Update register.js

--- register.js
+++ register.js
@@ -9,7 +9,7 @@ document.getElementById("registerBtn").onclick = async () => {
   }
 
   try {
-    const res = await fetch("/register-community", {
+    const res = await fetch("https://nexora-systems-worker.nexora-systems.workers.dev/register-community", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ name, code, ownerEmail }),
